290bcbbc0d6b5c5a8b50137cef3448b788ab7d83
slymax <contact@slymax.com>
2018-07-10 21:05:52 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+This is a template project for Android Studio that allows you to create an android webview application in minutes. You can use it to create a simple app for your website or as a starting point for your HTML5 based android app.
+
+### Getting started
+
+[Download](https://github.com/slymax/webview/archive/master.zip) or clone this repository and import it into Android Studio.
+
+### Using a remote source
+
+If you want to create an app that displays the contents of a remote website
+
+1. uncomment lines **30** and **31** in `MainActivity.java` and replace `http://example.com` with your remote source
+
+	```
+	mWebView.loadUrl("http://example.com");
+	mWebView.setWebViewClient(new MyWebViewClient());
+	```
+
+2. open the `MyWebViewClient.java` file and replace `example.com` on line **12** with your custom hostname
+
+	```
+	if (Uri.parse(url).getHost().endsWith("example.com")) {
+	```
+
+### Using a local source
+
+If you want to create a local HTML5 android app
+
+1. uncomment line **34** in `MainActivity.java`
+
+	```
+	mWebView.loadUrl("file:///android_asset/index.html");
+	```
+
+2. put all your files (including your `index.html`) in the `assets` directory
---
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+
+        // Force links and redirects to open in the WebView instead of in a browser
+        mWebView.setWebViewClient(new WebViewClient());
+
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // REMOTE RESOURCE
+        // mWebView.loadUrl("http://example.com");
+        // mWebView.setWebViewClient(new MyWebViewClient());
+
+        // LOCAL RESOURCE
+        // mWebView.loadUrl("file:///android_asset/index.html");
+    }
+
+    // Prevent the back-button from closing the app
+    @Override
+    public void onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+}
